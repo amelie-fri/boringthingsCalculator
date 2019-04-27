6126fcf3c6361fdae066e5d90ba0b9189678301c
Smule <markus.huse@gmail.com>
2019-04-28 00:20:05 +0200

    Improved comments. Fixed random color for background

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -14,19 +14,11 @@ var ranColor = {
 var buttonArray = [];
 var displayArray = [];
 
-
-function LoadRanCol()  {
-	ranColor.r = random(255);
-	ranColor.g = random(255);
-	ranColor.b = random(255);
-}
-
 function preload () {
 	buttonPic = loadImage('data/Knopf3.gif');
 	machinePic= loadImage('data/Rechenmaschine_neu6.gif');
 	soundFormats('mp3');
 	buttonSound = loadSound('data/sound.mp3');
-
 }
 
 function setup() {;
@@ -38,27 +30,32 @@ function setup() {;
 
   calculator = new picture(imgPosX,imgPosY, machinePic);
 
+  /** Construct button classes. Last variable refers to the index of the connected button (to the left) button 1 is all the way to the left */
   button1 = new button(57, buttonPic,buttonSound,null);
-  button2 = new button(57, buttonPic,buttonSound,1);
-  button3 = new button(57, buttonPic,buttonSound,2);
-  button4 = new button(57, buttonPic,buttonSound,3);
-  button5 = new button(57, buttonPic,buttonSound,4);
-  button6 = new button(57, buttonPic,buttonSound,5);
+  button2 = new button(57, buttonPic,buttonSound,0);
+  button3 = new button(57, buttonPic,buttonSound,1);
+  button4 = new button(57, buttonPic,buttonSound,2);
+  button5 = new button(57, buttonPic,buttonSound,3);
+  button6 = new button(57, buttonPic,buttonSound,4);
 
+  /** Create button array */
   buttonArray.push(button1,button2,button3,button4,button5,button6)
 
+  /** Construct display classes */
   display1 = new display(50, 25, 20); // round edge does not work
   display2 = new display(50, 25, 20);
   display3 = new display(50, 25, 20);
   display4 = new display(50, 25, 20);
   display5 = new display(50, 25, 20);
   display6 = new display(50, 25, 20);
-
+  
+  /** Create display array. The index refers to the button with the same index in buttonarray */
   displayArray.push(display1,display2,display3,display4,display5,display6);
 }
 
 
 function draw() {
+	/** Draws the Background */
 	background(ranColor.r, ranColor.g, ranColor.b);
 	
 	/** Show the Machine */
@@ -90,6 +87,8 @@ function mouseClicked() {
 		/** Calls the Button pressed function for each object with the connected display */
 		ButtonPressed(buttonElement,displayArray[i]);
 	});
+	/** Loads new random colors */
+	LoadRanCol();
 }
 
 function ButtonPressed(PressedButton,ConnectedDisplay){
@@ -105,7 +104,7 @@ function RotateConnectedButton(PressedButton){
 	/** Checks if we have connected an overflow button */
 	if (PressedButton.getConnectedIndex() != null){
 		/** Gets the connected button */
-		var ConnectedButton = buttonArray[PressedButton.getConnectedIndex()-1];
+		var ConnectedButton = buttonArray[PressedButton.getConnectedIndex()];
 		/** Rotate the connected button and check for overflow. Reenters the function again if the connected button overflows */
 		if (ConnectedButton.rotateRight()){
 			RotateConnectedButton(ConnectedButton);
@@ -124,4 +123,10 @@ function keyPressed() {
 		buttonSound.setVolume(0.4);
 		buttonSound.play();
 	}
+}
+
+function LoadRanCol()  {
+	ranColor.r = random(255);
+	ranColor.g = random(255);
+	ranColor.b = random(255);
 }
